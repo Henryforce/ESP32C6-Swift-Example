97f4e8e2c2dfbeb5c3d223d0be94bf74f7b4cc18
Henryforce <hj_serrano@hotmail.com>
2024-07-09 19:31:18 +0900

    Added app example to read sensor data on Apple platforms (#9)
    * Added app example to read sensor data on Apple platforms
    
    * Updated reactive code for reading all sensor data

---
+++ ESP32C6Example/ESP32C6Example/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  ESP32C6Example
+//
+//  Created by Henry Javier Serrano Echeverria on 7/7/24.
+//
+
+import UIKit
+import BLECombineKit
+import CoreBluetooth
+import Combine
+
+@MainActor
+final class ViewController: UIViewController {
+  
+  var centralManager: BLECentralManager?
+  var disposables = Set<AnyCancellable>()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+    centralManager = BLECombineKit.buildCentralManager(with: CBCentralManager())
+    
+    // TODO: enable in future commits, but make sure to connect and disconnect.
+    // setupButton()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+      self.readData()
+    }
+  }
+  
+  private func setupButton() {
+    let action = UIAction(title:"Read data", handler: { [weak self] _ in
+      self?.readData()
+    })
+    let button = UIButton(primaryAction: action)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(button)
+    
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+    ])
+  }
+  
+  // TODO: disconnect after reading all data.
+  private func readData() {
+    guard let centralManager else { return }
+    
+    let serviceUUID = CBUUID(string: "0x00FF")
+    let mainStream = centralManager.scanForPeripherals(
+      withServices: [serviceUUID],
+      options: nil
+    )
+    
+    let characteristicsStream = mainStream
+      .first()
+      .flatMap { $0.peripheral.connect(with: nil) }
+      .flatMap { $0.discoverServices(serviceUUIDs: [serviceUUID]) }
+      .flatMap { $0.discoverCharacteristics(characteristicUUIDs: nil) }
+      .share()
+      .eraseToAnyPublisher()
+    
+    observeCharacteristic(for: characteristicsStream, uuidString: "0xFF01", name: "Ambient Light")
+    observeCharacteristic(for: characteristicsStream, uuidString: "0xFF02", name: "UV Index")
+    observeCharacteristic(for: characteristicsStream, uuidString: "0xFF03", name: "Temperature")
+    observeCharacteristic(for: characteristicsStream, uuidString: "0xFF04", name: "Humidity")
+  }
+  
+  private func observeCharacteristic(
+    for stream: AnyPublisher<BLECharacteristic, BLEError>,
+    uuidString: String,
+    name: String
+  ) {
+    // Create an AsyncThrowingPublisher from a Publisher stream which can then
+    // be awaited in a Task.
+    let asyncStream = stream
+        .filter { $0.value.uuid == CBUUID(string: uuidString) }
+        .flatMap { $0.observeValue() }
+        .first()
+        .values
+    
+    Task {
+      for try await data in asyncStream {
+        print("\(name): \(data.uintValue?.correctBytes ?? 0)")
+      }
+      print("Finished reading \(name)")
+    }
+  }
+  
+}
+
+extension UInt32 {
+  // TODO: modify the firmware side to send this data properly.
+  var correctBytes: UInt32 {
+    var value: UInt32 = 0
+    value |= (self & 0xFF000000) >> 24
+    value |= (self & 0xFF0000) >> 8
+    value |= (self & 0xFF00) << 8
+    value |= (self & 0xFF) << 24
+    return value
+  }
+}
